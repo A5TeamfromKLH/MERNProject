1308992b48932bc8033b13ee3519b116a456d9db
A5TeamfromKLH <167906032+A5TeamfromKLH@users.noreply.github.com>
2024-04-24 13:05:19 +0530

    Add files via upload

---
+++ pages/payment.jsx
@@ -0,0 +1,3 @@
+<div>
+               <button><form><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_N4alLMIlynqElV" async> </script> </form></button>
+               </div>
